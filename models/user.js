d3e6fa5549f01805aed84cbb8922c026794022d8
minatore06 <stei2002@hotmail.it>
2023-08-11 02:02:30 +0200

    still working on db models

--- models/user.js
+++ models/user.js
@@ -1,9 +1,11 @@
 const { ObjectId } = require('mongodb');
 const { connectDB, closeDB } = require('../connection');
+const randomString = require('../utils/random_string')
 
 class User {
     constructor(email, password, ruolo) {
         if (!email || !password)
+            return null
 
         this._id = new ObjectId();
         this.email = email;
@@ -13,7 +15,9 @@ class User {
         this.signupDate = Date.now();
         this.totalSpace = 1;
         this.paidSpace = 0;
-        //referal
+        do {
+            this.referal = randomString(8, 'aA#');
+        } while (User.findOne({referal:this.referal}));
         this.status = "Active";//Active;Blocked;Deactiveted;Deleted
         //autorizzazioni
         //log utente
@@ -26,6 +30,67 @@ class User {
             const db = await connectDB();
             const usersCollection = db.collection('users');
             const result = usersCollection.insertOne(user);
+            closeDB();
+            return result;
+        } catch (err) {
+            console.error('Errore nel recupero dati db:'+err);
+            throw err;
+        }
+    }
+
+    static async findOne(query) {
+        try {
+            const db = await connectDB();
+            const usersCollection = db.collection('users');
+            const result = usersCollection.findOne(query);
+            closeDB();
+            return result;
+        } catch (err) {
+            console.error('Errore nel recupero dati db:'+err);
+            throw err;
+        }
+    }
+    static async find(query) {
+        try {
+            const db = await connectDB();
+            const usersCollection = db.collection('users');
+            const result = usersCollection.find(query);
+            closeDB();
+            return result;
+        } catch (err) {
+            console.error('Errore nel recupero dati db:'+err);
+            throw err;
+        }
+    }
+    static async countDocuments(query) {
+        try {
+            const db = await connectDB();
+            const usersCollection = db.collection('users');
+            const result = usersCollection.countDocuments(query);
+            closeDB();
+            return result;
+        } catch (err) {
+            console.error('Errore nel recupero dati db:'+err);
+            throw err;
+        }
+    }
+    static async updateOne(query, update) {
+        try {
+            const db = await connectDB();
+            const usersCollection = db.collection('users');
+            const result = usersCollection.updateOne(query, update);
+            closeDB();
+            return result;
+        } catch (err) {
+            console.error('Errore nel recupero dati db:'+err);
+            throw err;
+        }
+    }
+    static async deleteOne(query) {
+        try {
+            const db = await connectDB();
+            const usersCollection = db.collection('users');
+            const result = usersCollection.deleteOne(query);
             closeDB();
             return result;
         } catch (err) {
