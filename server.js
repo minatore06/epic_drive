1490a743238c03db4beb9375d548519faa018087
minatore06 <stei2002@hotmail.it>
2023-10-04 17:58:36 +0200

    test3

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.use(session({
     secret:process.env.SESSION_SECRET,
     resave:false,
     saveUninitialized: true,
-    cookie: {secure: true, httpOnly: true, sameSite:'strict', maxAge: 60 * 30 * 1000}//cambiare a true quando impostato https
+    cookie: {secure: false, httpOnly: true, sameSite:'strict', maxAge: 60 * 30 * 1000}//cambiare a true quando impostato https
 }));
 app.use(helmet.contentSecurityPolicy({
     directives: {
