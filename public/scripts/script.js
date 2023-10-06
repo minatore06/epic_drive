17d5f503d4f0bdd9ab7fb9dbe16ff84bb4c9bcf5
minatore06 <stei2002@hotmail.it>
2023-10-06 12:54:30 +0200

    typo

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -201,7 +201,7 @@ function create_folder(path) {
             let response = JSON.parse(xmlHttp.response);
             if (response.message == "missing CSRF token" || response.message == "CSRF token invalid")
                 logout();
-            alert("Access denied " + response.message);
+            alert("Access denied");
         }
     }
     xmlHttp.open("POST", url, true)
