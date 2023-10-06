5acabea7d26a67a32ed2d061612c724f53260a10
minatore06 <stei2002@hotmail.it>
2023-10-06 18:04:22 +0200

    I think this way it should work

--- server.js
+++ server.js
@@ -318,10 +318,11 @@ function generateCSRFToken(req, res){
         httpOnly: true,
         signed: true
     }
+    let csrfToken = require('crypto').randomBytes(128).toString('hex');
 
-    req.session.csrfToken = require('crypto').randomBytes(128).toString('hex');
-    res.cookie('_csrf_token', req.session.csrfToken, options);
-    res.cookie('_csrf_hashed', require('crypto').createHash('sha256').update(req.session.csrfToken+process.env.CSRF_SECRET, 'binary').digest('base64'), options)
+    req.session.csrfHash = require('crypto').createHash('sha256'); 
+    res.cookie('_csrf_token', csrfToken, options);
+    res.cookie('_csrf_hashed', csrfHash.update(csrfToken+process.env.CSRF_SECRET, 'binary').digest('base64'), options)
 }
 
 //MIDDLEWARE
@@ -358,9 +359,9 @@ function authenticateToken(req, res, next){
 }
 
 function checkCSRFToken(req, res, next) {
-    if (!req.signedCookies['_csrf_hashed'] || !req.signedCookies['_csrf_token'])
+    if (!req.signedCookies['_csrf_hashed'] || !req.signedCookies['_csrf_token'] || !req.session.csrfHash)
         return res.status(403).json({message: 'missing CSRF token'});
-    if (require('crypto').createHash('sha256').update(req.signedCookies['_csrf_token']+process.env.CSFT_SECRET, 'binary').digest('base64') !== req.signedCookies['_csrf_hashed'])
+    if (req.session.csrfHash.update(req.signedCookies['_csrf_token']+process.env.CSFT_SECRET, 'binary').digest('base64') !== req.signedCookies['_csrf_hashed'])
         return res.status(403).json({message: 'CSRF token invalid'});
     next();
 }
