d104641206ad40fbb19b99baa7af272b72d31937
minatore06 <stei2002@hotmail.it>
2023-10-06 21:34:40 +0200

    typo

--- server.js
+++ server.js
@@ -361,7 +361,7 @@ function authenticateToken(req, res, next){
 function checkCSRFToken(req, res, next) {
     if (!req.signedCookies['_csrf_hashed'] || !req.signedCookies['_csrf_token'])
         return res.status(403).json({message: 'missing CSRF token'});
-    if (CSRF_HASH.copy().update(req.signedCookies['_csrf_token']+process.env.CSFT_SECRET, 'binary').digest('base64') !== req.signedCookies['_csrf_hashed'])
+    if (CSRF_HASH.copy().update(req.signedCookies['_csrf_token']+process.env.CSRF_SECRET, 'binary').digest('base64') !== req.signedCookies['_csrf_hashed'])
         return res.status(403).json({message: 'CSRF token invalid'});
     next();
 }
